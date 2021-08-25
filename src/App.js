54534782b37cbb6aa1a207b24b30e36e451c48cc
imoempire <mrimoempire@gmail.com>
2021-08-25 11:53:47 +0000

    edit new main

--- src/App.js
+++ src/App.js
@@ -4,37 +4,7 @@ import ContactForm from "./Components/ContactForm";
 import ContactsList from "./Components/ContactsList";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contact: [
-        { Name: 'John',
-          Phone: '0244945255',
-          Location: 'Accra'}
-      ],
-    };
-  }
-  handleAddContact = (newContact) => {
-    newContact.id = Math.random().toString();
-    this.setState({
-      contact: [...this.state.contact, newContact],
-    });
-  };
-
-  handleDeleteContact = (contactsId) => {
-    const newArr = this.state.contact.filter((contacts) => {
-      return contacts.id !== contactsId;
-    });
-    this.setState({ contact: newArr });
-  };
-  handleEditContact=(updatedContacts)=>{
-  this.setState({
-    contact: this.state.contact.map((contacts) =>
-    contacts.id === updatedContacts.id ? updatedContacts : contacts 
-    ),
-  });
-  };
-
+  
   render() {
     return (
       <div className="App">
